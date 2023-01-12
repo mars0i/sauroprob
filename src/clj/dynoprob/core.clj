6e8b180e285121a4f87b725465c6d7c5279d625d
Marshall Abrams <marshall@logical.net>
2023-01-12 16:28:12 -0600

    Added logistic functions, removed foo fn

--- src/clj/dynoprob/core.clj
+++ src/clj/dynoprob/core.clj
@@ -1,6 +1,22 @@
 (ns dynoprob.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn logistic
+  "The logistic function with parameter r."
+  [r x]
+  (* r x (- 1 x)))
+
+(def logistic-4
+  "([x])
+   Applies a logistic function with parameter r=4 to x."
+  (partial logistic 4))
+
+(defn logistic-vals
+  "Returns a lazy sequence of values from iterating a logistic
+  equation with parameter r, beginning with given initial state."
+  [r initial]
+  (iterate (partial logistic r) initial))
+
+(comment
+  (def xs4 (logistic-vals 4 0.25))
+
+) 
