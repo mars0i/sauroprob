7d731e7eaeee635ede89dda2321cbf7304b86880
Marshall Abrams <marshall@logical.net>
2023-01-12 16:17:10 -0600

    My standard initial setup

--- project.clj
+++ project.clj
@@ -13,4 +13,7 @@
                  [metasoarous/oz "1.6.0-alpha36"] ; As of 1/12/2023 site says "Please use 1.6.0-alpha36 for the most recent stable version of Oz. "
                  [cljplot "0.0.2a-SNAPSHOT"]
                 ]
-  :repl-options {:init-ns dynoprob.core})
+  :source-paths ["src/clj"]
+)
+
+ ;; :repl-options {:init-ns dynoprob.core})
