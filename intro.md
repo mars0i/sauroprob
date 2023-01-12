c1b39c92c0b858c80d541a9416c7e968ca3af905
Marshall Abrams <marshall@logical.net>
2023-01-12 16:01:01 -0600

    Initial commit after adding 'lein new' files

---
+++ intro.md
@@ -0,0 +1,3 @@
+# Introduction to dynoprob
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dynoprob/core_test.clj
@@ -0,0 +1,7 @@
+(ns dynoprob.core-test
+  (:require [clojure.test :refer :all]
+            [dynoprob.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
