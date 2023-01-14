6efc82fd7c34f47e13c8491336d4ddb0995c3a63
Marshall Abrams <marshall@logical.net>
2023-01-14 15:22:46 -0600

    Replaced find-cycle with new version returning info in map

--- src/clj/sauroprob/core.clj
+++ src/clj/sauroprob/core.clj
@@ -17,30 +17,7 @@
   [r initial]
   (iterate (partial logistic r) initial))
 
-;; FIXME The period is not the size of the set, because there may be
-;; an initial sequence before the recurrence begins.
-;; OBSOLETE
 (defn find-cycle
-  "Loops through the values in sequence xs, looking for the first 
-  value that has already appeared in the sequence.  If such a value
-  is found, a pair containing the value and the period of the number
-  of preceding values (the period) is returned.  If the end
-  of the sequence is encountered with no repeats, nil is returned.
-  (Note that the test for recurrence uses a clojure set, which uses
-  the function hash to determine identity.  This means that numbers
-  that appear to be equal but that are of different numeric data
-  types may or may not be treated as identical.  To prevent problems,
-  make sure elements in the sequence are all of the same data type.)"
-  [xs]
-  (loop [ys xs, seen #{}]
-    (let [y (first ys)]
-      (cond (empty? ys) nil
-            (seen y)    [y (count seen)] ; FIXME
-            :else (recur (rest ys) (conj seen y))))))
-
-
-;; FIXME off by one. TODO: Add index of start of cycle.
-(defn find-cycle2
   "Loops through the values in sequence xs, looking for the first value
   that has already appeared in the sequence.  If the value is found, a
   pair containing the value and the period (current index - previous) is
@@ -57,15 +34,16 @@
       (let [y (first ys)
             prev-idx (seen y)]
         (if prev-idx 
-          [y (- i prev-idx)] ; TODO off-by-one?
+          {:val y :period (- i prev-idx) :starts-at prev-idx}  ; old version: [y (- i prev-idx) prev-idx]
           (recur (rest ys) (inc i) (assoc seen y i)))))))
 
 (comment
   (def xs4 (logistic-vals 4 0.5))
   (take 10 xs4)
   (find-cycle2 xs4)
-  (def xs (concat (range 12) [11] (range 5 10)))
+  (def xs (concat [7] (range 5) (range 5 1 -1)))
   (find-cycle2 xs)
+  (find-cycle2 (range 1000))
 
   (def xs4 (logistic-vals 4 0.76))
   (take 50 xs4)
