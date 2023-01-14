6db4471087af310ae514dccb54d48b03398cfde1
Marshall Abrams <marshall@logical.net>
2023-01-14 15:26:41 -0600

    Updated docstring for new functionality of find-cycle

--- src/clj/sauroprob/core.clj
+++ src/clj/sauroprob/core.clj
@@ -20,13 +20,14 @@
 (defn find-cycle
   "Loops through the values in sequence xs, looking for the first value
   that has already appeared in the sequence.  If the value is found, a
-  pair containing the value and the period (current index - previous) is
-  returned.  If the end of the sequence is encountered with no repeats,
-  nil is returned.  (Note that the test for recurrence uses a clojure
+  map is returned, otherwise nil if no cycles are found.  The map has
+  keys :val for the number that cycles, :period for the period of the
+  cycle, and :starts-at for the zero-based index of the value that
+  starts the cycle. Note that the test for recurrence uses a clojure
   map, which uses the function 'hash' to determine identity.  This means
   that numbers that appear to be equal but that are of different numeric
-  data types may or may not be treated as identical.  The user should 
-  insure that elements in the sequence are all of the same type.)"
+  data types may or may not be treated as identical.  The user should
+  insure that elements in the sequence are all of the same type."
   [xs]
   (loop [ys xs, i 0, seen {}]  ; seen is "inverted vector": look up by val, returns index
     (if (empty? ys)
@@ -39,11 +40,12 @@
 
 (comment
   (def xs4 (logistic-vals 4 0.5))
+  (def xs4 (logistic-vals 4 0.501))
   (take 10 xs4)
-  (find-cycle2 xs4)
+  (find-cycle xs4)
   (def xs (concat [7] (range 5) (range 5 1 -1)))
-  (find-cycle2 xs)
-  (find-cycle2 (range 1000))
+  (find-cycle xs)
+  (find-cycle (range 1000))
 
   (def xs4 (logistic-vals 4 0.76))
   (take 50 xs4)
