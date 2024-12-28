dc43f6b94f8a26c9a1c91c3f5bcfe9b4f79d3cbb
Marshall Abrams <marshall@logical.net>
2024-12-27 21:50:12 -0600

    Upgraded to Clojure 1.12, removed unused dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/math.numeric-tower "0.0.5"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/data.json "2.4.0"]
-                 [generateme/fastmath "2.1.8"] ; or 2.1.9-SNAPSHOT
+                 ;[generateme/fastmath "2.1.8"] ; or 2.1.9-SNAPSHOT
                  [aerial.hanami "0.17.0"]
                  [techascent/tech.viz "6.00-beta-16-4"]
                  [io.github.nextjournal/clerk "0.12.707"]
