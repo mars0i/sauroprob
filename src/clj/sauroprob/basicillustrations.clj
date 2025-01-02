8922fd49ca80b399cffea243b74f95f5a6774ed9
Marshall Abrams <marshall@logical.net>
2025-01-02 14:41:29 -0600

    Moved code in core.clj to new files

---
+++ src/clj/sauroprob/basicillustrations.clj
@@ -0,0 +1,65 @@
+(ns sauroprob.core
+  (:require 
+            ;[clojure.math.numeric-tower :as m]
+            [clojure.math :as m] ; new in Clojure 1.11 
+            [oz.core :as oz]
+            [aerial.hanami.common :as hc]
+            [aerial.hanami.templates :as ht]
+            [utils.json :as json]
+            [utils.string :as st]
+            [utils.misc :as msc]
+            [utils.math :as um]
+            [sauroprob.hanami :as sh]
+            ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(oz/start-server!)
+
+(comment
+
+  ;; Four moran1959 plots
+  (def four-moran-specs [(sh/make-vl-spec 0.0 3.0 um/moran1950 1.5 1 [0.01] 10 1.0)
+                         (sh/make-vl-spec 0.0 3.0 um/moran1950 2.0 1 [0.01 0.1 0.8 2.0] 10 1.0)
+                         (sh/make-vl-spec 0.0 3.0 um/moran1950 2.5 2 [0.01] 10 1.0)
+                         (sh/make-vl-spec 0.0 3.0 um/moran1950 3.0 1 [0.01] 10 1.0)])
+  (def grid-spec (hc/xform sh/grid-chart :COLUMNS 2 :ROWS 2 :CONCAT four-moran-specs))
+  (oz/view! grid-spec)
+
+  (def yo (sh/neg-one-line 0.0 1.0 (um/logistic 2.0) 0.5))
+
+  ;; Checking whether apparent logistic fixed points are in fact:
+  (um/logistic 2 0.5)
+  (um/logistic 3 0.6667)
+  (um/logistic 4 0.75)
+  (um/logistic 4.5 0.77)
+
+  ;; Four logistic plots
+  (def four-logistic-specs [(sh/make-vl-spec 0.0 1.0 um/logistic 2.0 3 [0.01 0.1 0.2 0.7 0.8] 10 0.5)
+                            (sh/make-vl-spec 0.0 1.0 um/logistic 3.0 2 [0.01 0.5 0.9] 10 0.66667)
+                            (sh/make-vl-spec 0.0 1.0 um/logistic 4.0 1 [0.01 0.2 0.5 0.8] 10 0.75)
+                            (sh/make-vl-spec -0.2 1.2 um/logistic 4.5 1 [0.01] 10 0.77)]) ; note different x-min, x-max
+  (def grid-spec (hc/xform sh/grid-chart :COLUMNS 2 :ROWS 2 :CONCAT four-logistic-specs))
+  (oz/view! grid-spec)
+
+  (oz/view! (sh/make-vl-spec -0.2 1.2 um/logistic 4.5 1 [0.3] 10 0.77)) ; note different x-min, x-max
+
+  (def grid-spec (hc/xform sh/grid-chart :COLUMNS 2 :ROWS 4 :CONCAT (concat four-moran-specs four-logistic-specs)))
+  (oz/view! grid-spec)
+
+
+  (def some-specs [(sh/make-vl-spec 0.0 3.0 um/moran1950 1.25 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 4.5 um/moran1950 1.5 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 4.5 um/moran1950 2.0 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 4.5 um/moran1950 2.5 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 4.5 um/moran1950 3.0 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 4.5 um/moran1950 3.5 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 4.5 um/moran1950 3.75 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 1.0 um/logistic 1.5 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 1.0 um/logistic 2.0 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 1.0 um/logistic 3.0 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 1.0 um/logistic 4.0 1 [0.01] 10)
+                   (sh/make-vl-spec 0.0 1.2 um/logistic 4.5 1 [0.01] 10)]) ; note different x-max
+  (def grid-spec (hc/xform sh/grid-chart :COLUMNS 3 :ROWS 4 :CONCAT some-specs))
+  (oz/view! grid-spec)
+)
--- src/clj/sauroprob/mayoster1976.clj
+++ src/clj/sauroprob/mayoster1976.clj
@@ -1,4 +1,6 @@
-(ns sauroprob.core
+;; Plots illustrating ideas from May and Oster's "Bifurcations and dynamical
+;; complexity in simple ecological models", _The American Naturalist_ 1976.
+(ns sauroprob.mayoster1976
   (:require 
             ;[clojure.math.numeric-tower :as m]
             [clojure.math :as m] ; new in Clojure 1.11 
@@ -18,6 +20,17 @@
 (comment
   (oz/start-server!)
 
+  ;; Illustration of methods.  Note that for this family of functions, the
+  ;; last optional arg, which is the x coord of F^1 fixed point, is always
+  ;; the same, 1.0.
+  (def four-moran-specs [(sh/make-vl-spec 0.0 3.0 um/moran1950 1.5 1 [0.01] 0 1.0)
+                         (sh/make-vl-spec 0.0 3.0 um/moran1950 2.0 1 [0.01 0.1 0.8 2.0] 10 1.0)
+                         (sh/make-vl-spec 0.0 3.0 um/moran1950 2.5 2 [0.01] 3 1.0)
+                         (sh/make-vl-spec 0.0 3.0 um/moran1950 3.0 1 [0.01] 10 1.0)])
+  (def grid-spec (hc/xform sh/grid-chart :COLUMNS 2 :ROWS 2 :CONCAT four-moran-specs))
+  (oz/view! grid-spec)
+
+  ;; Illustrates discussion on page 578
   (oz/view! (hc/xform sh/grid-chart :COLUMNS 2 :ROWS 5 :CONCAT 
                       [(sh/make-vl-spec 0.0 3.0 um/moran1950 1.0  2 [] 0 1.0)
                        (sh/make-vl-spec 0.0 3.0 um/moran1950 1.1  2 [] 0 1.0)
@@ -30,14 +43,10 @@
                        (sh/make-vl-spec 0.0 3.0 um/moran1950 2.5  2 [] 0 1.0)
                        (sh/make-vl-spec 0.0 3.0 um/moran1950 2.7  2 [] 0 1.0)
                        (sh/make-vl-spec 0.0 3.0 um/moran1950 3.0  2 [] 0 1.0)
+                       (sh/make-vl-spec 0.0 3.0 um/moran1950 3.5  2 [] 0 1.0)
+                       (sh/make-vl-spec 0.0 3.0 um/moran1950 4.0  2 [] 0 1.0)
+                       (sh/make-vl-spec 0.0 3.0 um/moran1950 5.0  2 [] 0 1.0)
                       ]))
 
-  ;; Four moran1959 plots
-  (def four-moran-specs [(sh/make-vl-spec 0.0 3.0 um/moran1950 1.5 1 [0.01] 0 1.0)
-                         (sh/make-vl-spec 0.0 3.0 um/moran1950 2.0 1 [0.01 0.1 0.8 2.0] 10 1.0)
-                         (sh/make-vl-spec 0.0 3.0 um/moran1950 2.5 2 [0.01] 10 1.0)
-                         (sh/make-vl-spec 0.0 3.0 um/moran1950 3.0 1 [0.01] 10 1.0)])
-  (def grid-spec (hc/xform sh/grid-chart :COLUMNS 2 :ROWS 2 :CONCAT four-moran-specs))
-  (oz/view! grid-spec)
-
 )
+
