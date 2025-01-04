b577ba016c9f7b7a16487feb795d6ac1e8a2e2ee
Marshall Abrams <marshall@logical.net>
2025-01-04 15:42:36 -0600

    New, modified examples

--- src/clj/sauroprob/core.clj
+++ src/clj/sauroprob/core.clj
@@ -18,16 +18,9 @@
 (comment
   (oz/start-server!)
 
-  (oz/view! (sh/make-vl-spec 0.0 1.0 um/logistic 4.25 1
+  (oz/view! (sh/make-vl-spec 0.0 1.0 um/logistic 4.25 2
                              [0.05 0.075 0.15] 3
                              :fixedpt-x 0.5
                              :addl-plots [(sh/horiz 1.0)]))
-
-  ;; fixing aspect ration:
-  ;; Maybe something like `{:y scale = {domain = (200,300)}}` but the
-  ;; correct syntax. cf. forage/viz/hanami.clj:
-  ;;             :XSCALE {"domain" [data-bound-min data-bound-max]}
-  ;;             :YSCALE {"domain" [data-bound-min data-bound-max]}
- 
 
 )
--- src/clj/sauroprob/mayoster1976.clj
+++ src/clj/sauroprob/mayoster1976.clj
@@ -48,5 +48,7 @@
                        (sh/make-vl-spec 0.0 3.0 um/moran1950 5.0  2 [0.25] 4 :fixedpt-x 1.0)
                       ]))
 
+  (oz/view! (sh/make-vl-spec 0.0 3.0 um/moran1950 2.2 2 [0.5] 20 :fixedpt-x 1.0))
+
 )
 
