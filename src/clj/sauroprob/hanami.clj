6b87dfefd70f97c7abded755abe2d54f2dbebb59
Marshall Abrams <marshall@logical.net>
2025-01-24 10:25:57 -0600

    Deleted obsolete comment

--- src/clj/sauroprob/hanami.clj
+++ src/clj/sauroprob/hanami.clj
@@ -163,7 +163,6 @@
              :COLOR "label"
              :SIZE 0.5)))
 
-;; TODO Make params into a (possibly empty) sequence of parameters.
 ;; TODO Replace number of compositions with a sequence of composition numbers.
 ;; NOTE This inefficiently recalculates the same values in each composed function.
 (defn make-vl-spec 
