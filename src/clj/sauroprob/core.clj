d7cea38222bc38721d04bf13a065f7174cac2d27
Marshall Abrams <marshall@logical.net>
2025-01-25 14:59:55 -0600

    Added new simple logistic plot

--- src/clj/sauroprob/core.clj
+++ src/clj/sauroprob/core.clj
@@ -18,7 +18,9 @@
 (comment
   (oz/start-server!)
 
-  (oz/view! (sh/make-vl-spec 0.0 1.0 um/logistic 3.00 6
+  (oz/view! (sh/make-vl-spec 0.0 1.0 um/logistic [3.0] 1 [] 1))
+
+  (oz/view! (sh/make-vl-spec 0.0 1.0 um/logistic [3.0] 6
                              [0.05 0.075 0.15] 3
                              :fixedpt-x 0.5
                              :addl-plots [(sh/horiz 1.0)]))
