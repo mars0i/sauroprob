09b130da069dbfb9df92f6fc3f488b007d4bab7e
mabrams <mabrams001@bham.rr.com>
2025-02-09 17:58:04 -0600

    Commented out unused requires

--- src/clj/sauroprob/mayoster1976.clj
+++ src/clj/sauroprob/mayoster1976.clj
@@ -7,8 +7,6 @@
             ;[oz.core :as oz]
             [aerial.hanami.common :as hc]
             [aerial.hanami.templates :as ht]
-            [utils.json :as json]
-            [utils.string :as st]
             [utils.misc :as msc]
             [utils.math :as um]
             [sauroprob.hanami :as sh]
