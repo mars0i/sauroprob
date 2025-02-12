d0a830455d6da3807dfc23401e16f7da7dc3f3e4
mabrams <mabrams001@bham.rr.com>
2025-02-12 16:57:57 -0600

    bugfixes and comments

--- src/clj/sauroprob/devaney.clj
+++ src/clj/sauroprob/devaney.clj
@@ -11,18 +11,26 @@
 
 ;; ### $\lambda < 0$,  $E_\lambda^2$:
 
+;; Note $E'_\lambda (x) = (\lambda e^x)' = \lambda e^x$, and 
+;; $(F_\lambda^2)' = (\lambda e^{\lambda e^x})' = \lambda e^x \lambda e^{\lambda e^x}$. 
+
+;; $E_\lambda^2$:
 (defn scaled-exp-2
   ([lambda] (partial scaled-exp-2 lambda))
   ([lambda x] (um/scaled-exp lambda (um/scaled-exp lambda x))))
 
 ;; #### $-e < \lambda < 0$ (i.e. $\lambda = -e + 2$):, $E_\lambda^2$:
-(kind/vega-lite (sh/make-vl-spec [-1.4 1.0] scaled-exp-2 [(+ (- m/E) 2.0)] [] [] 5 :y-lims [-0.8 -0.0]))
+(kind/vega-lite (sh/make-vl-spec [-1.4 1.0] scaled-exp-2 [(+ (- m/E) 2.0)] [1] [-1.4 0.20] 5 :y-lims [-0.8 -0.0]))
+
+;; For $\lambda < -e$, $\hat{x} = E_\lambda^2(\hat{x}) = \lambda e^{\lambda
+;; e^\hat{x}} = \lambda \exp(\lambda \exp\hat{x})$.
+
 
 ;; #### $\lambda < -e$ (i.e. $\lambda = -e - 3$), $E_\lambda^2$::
-(kind/vega-lite (sh/make-vl-spec [-7.0 2.0] scaled-exp-2 [(- (- m/E) 3.0)] [] [] 5 :y-lims [-6.0 1.0]))
+(kind/vega-lite (sh/make-vl-spec [-7.0 2.0] scaled-exp-2 [(- (- m/E) 3.0)] [1] [-2 -1] 5 :y-lims [-7.0 1.0]))
 
 ;; #### $\lambda = e^{-1}$, $E_\lambda^2$:
-(kind/vega-lite (sh/make-vl-spec [-3.5 1.0] scaled-exp-2 [(- m/E)] [] [] 1 :y-lims [-3.0 1.0]))
+(kind/vega-lite (sh/make-vl-spec [-3.5 1.0] scaled-exp-2 [(- m/E)] [1] [] 1 :y-lims [-3.0 1.0]))
 
 ;; ## $\S 12.1$ $x_{n+1} = \lambda e^x$
 
