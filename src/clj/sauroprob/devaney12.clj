ff05ad8ef59b5b54d2d33e021aeed3373d217d7f
Marshall Abrams <marshall@logical.net>
2025-02-13 13:31:41 -0600

    Refined formatting

--- src/clj/sauroprob/devaney12.clj
+++ src/clj/sauroprob/devaney12.clj
@@ -1,4 +1,4 @@
-;; # Experiments to go with Devaney 3rd ed, chapter 12
+;; # Experiments to go with Devaney 3rd ed, ch. 12
 ;; ---
 ^:kindly/hide-code
 (ns sauroprob.devaney12
@@ -9,7 +9,9 @@
             [utils.math :as um]
             [sauroprob.hanami :as sh]))
 
-;; ### $\lambda < 0$,  $E_\lambda^2$:
+;; ## $\S 12.1$: $x_{n+1} = \lambda e^x$
+
+;; ### $\lambda < 0$,  $E_\lambda^2$
 
 ;; Note $E'_\lambda (x) = (\lambda e^x)' = \lambda e^x$, and 
 ;; $(F_\lambda^2)' = (\lambda e^{\lambda e^x})' = \lambda e^x \lambda e^{\lambda e^x}$. 
@@ -19,7 +21,7 @@
   ([lambda] (partial scaled-exp-2 lambda))
   ([lambda x] (um/scaled-exp lambda (um/scaled-exp lambda x))))
 
-;; #### $\lambda < -e$ (i.e. $\lambda = -e - 3$), $E_\lambda^2$::
+;; #### $\lambda < -e$ (i.e. $\lambda = -e - 3$), $E_\lambda^2$
 
 ;; What are the fixed points of $E_\lambda^2$?
 ;; For $\lambda < -e$, $\hat{x} = E_\lambda^2(\hat{x}) = 
@@ -70,28 +72,27 @@
 
 ;; ---
 
-;; #### $-e < \lambda < 0$ (i.e. $\lambda = -e + 2$):, $E_\lambda^2$:
+;; #### $-e < \lambda < 0$ (i.e. $\lambda = -e + 2$):, $E_\lambda^2$
 (kind/vega-lite (sh/make-vl-spec [-1.4 1.0] scaled-exp-2 [(+ (- m/E) 2.0)] [1] [-1.4 0.20] 5 :y-lims [-0.8 -0.0]))
 
 ;; ---
 
-;; #### $\lambda = e^{-1}$, $E_\lambda^2$:
+;; #### $\lambda = e^{-1}$, $E_\lambda^2$
 (kind/vega-lite (sh/make-vl-spec [-3.5 1.0] scaled-exp-2 [(- m/E)] [1] [] 1 :y-lims [-3.0 1.0]))
 
 ;; --
 
-;; ## $\S 12.1$ $x_{n+1} = \lambda e^x$
 
+;; ### $\lambda < 0$
 
-;; ### $\lambda < 0$:
-
-;; #### $-e < \lambda < 0$ (i.e. $\lambda = -e + 2$):
+;; #### $-e < \lambda < 0$, i.e. $\lambda = -e + 2$
 (kind/vega-lite (sh/make-vl-spec [-1.4 0.0] um/scaled-exp [(+ (- m/E) 2.0)] [1] [-0.6 -0.3] 5 :y-lims [-0.8 -0.0]))
 
-;; ## $\lambda < -e$ (i.e. $\lambda = -e - 2$):
+
+;; ### $\lambda < -e$, i.e. $\lambda = -e - 2$
 (kind/vega-lite (sh/make-vl-spec [-2.0 -0.4] um/scaled-exp [(- (- m/E) 2.0)] [1] [-1.4 -1.1] 5 :y-lims [-3.5 0.0]))
 
-;; #### $\lambda = -e$:
+;; #### $\lambda = -e$
 ;; These examples show how slow the approach to equilibrium is for $\lambda = -e$.
 ;; At $-e$, the slope is $-1$, so it's neither attracting nor repelling,
 ;; but because of the concavity, i.e. $f''<0$, at a distance from the fixed
@@ -104,16 +105,16 @@
 
 
 ;; ---
-;; ### $\lambda \geq 0$:
+;; ### $\lambda \geq 0$
 
-;; #### $\lambda = e^{-1}$:
+;; #### $\lambda = e^{-1}$
 (kind/vega-lite (sh/make-vl-spec [-4.0 2.0] um/scaled-exp [(/ m/E)] [1] [-0.5 1.70] 2 :y-lims [0.0 2.0]))
 
-;; #### $\lambda < e^{-1}$ (i.e. $\lambda = e^{-1} - 0.1$):
+;; #### $\lambda < e^{-1}$ (i.e. $\lambda = e^{-1} - 0.1$)
 (kind/vega-lite (sh/make-vl-spec [-4.0 2.0] um/scaled-exp [(- (/ m/E) 0.1)] [1] [-0.9 1.85] 6 :y-lims [0.0 2.0]))
 (kind/vega-lite (sh/make-vl-spec [-4.0 3.5] um/scaled-exp [(- (/ m/E) 0.1)] [1] [2.07] 5 :y-lims [0.0 8.0]))
 
-;; #### $\lambda > e^{-1}$ (i.e. $\lambda = e^{-1} + 0.1$):
+;; #### $\lambda > e^{-1}$ (i.e. $\lambda = e^{-1} + 0.1$)
 (kind/vega-lite (sh/make-vl-spec [-4.0 2.0] um/scaled-exp [(+ (/ m/E) 0.1)] [1] [0.2] 6 :y-lims [0.0 2.0]))
 
 
