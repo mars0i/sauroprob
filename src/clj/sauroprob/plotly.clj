2b484d31a66a87915f23c8f80d4c264b3198bbed
Marshall Abrams <marshall@logical.net>
2025-02-15 13:26:48 -0600

    More experiments

--- src/clj/sauroprob/plotly.clj
+++ src/clj/sauroprob/plotly.clj
@@ -1,3 +1,4 @@
+^:kindly/hide-code
 (ns sauroprob.plotly
   (:require [scicloj.tableplot.v1.plotly :as plotly]
             [tablecloth.api :as tc]
@@ -9,7 +10,7 @@
             [scicloj.kindly.v4.api :as kindly]
             [scicloj.metamorph.ml.rdatasets :as rdatasets]
             ;[aerial.hanami.templates :as ht]
-            ))
+           ))
 
 
 ;; This shows that Plotly lines are written in the order of points, and
@@ -19,13 +20,21 @@
 
 (-> data
     tc/dataset
+    ;; I haven't figured out how to set height/width in layer-line, but
+    ;; this works for those keys (but not :=margin):
+    (plotly/base {;:=height 400
+                  ;:=width 400
+                  ;:=margins {:t 25 :b 25 :l 25 :r 25}
+    })
     (plotly/layer-line {:=x :x
                         :=y :y
                         :=mark-color "purple"
-                        ;; ???
-                        :=layout {:=margin {:t 25 :b 25 :l 25 :r 25}
-                                  :=height 400
-                                  :=width 400}}))
+                        })
+    ;(assoc-in [:yaxes :scaleanchor] "x")
+    ;(assoc-in [:yaxes :scaleratio] 1)
+    kind/pprint
+    )
+
 
 
 ;; Where do these go?
@@ -34,8 +43,17 @@
          :=height 400
          :=width 400})
 
-;; Example from docs
-;; Why is it not flush with margins?
+;; Examples from docs
+
+(-> (rdatasets/datasets-iris)
+    (plotly/splom
+     {:=colnames [:sepal-length :sepal-width :petal-length :petal-width]
+      :=color :species
+      :=height 800
+      :=width 600}))
+
+;; Why is it not flush with margins?  If you remove the big spots, it's
+;; flush on left and right.
 (-> (rdatasets/ggplot2-economics_long)
     (tc/select-rows #(-> % :variable (= "unemploy")))
     (plotly/layer-point {:=x :date
@@ -57,5 +75,3 @@
       :=color :species
       :=mark-size 20
       :=mark-opacity 0.6}))
-
-
