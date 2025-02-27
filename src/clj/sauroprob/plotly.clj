5d336afe387a66b07ccee928f8e832c1af62786d
Marshall Abrams <marshall@logical.net>
2025-02-27 10:06:08 -0600

    Rehabbed f-dataset (formerly fn-dataset)

--- src/clj/sauroprob/plotly.clj
+++ src/clj/sauroprob/plotly.clj
@@ -12,36 +12,73 @@
 
 (def plot-steps 400)
 
-(defn fn-dataset
-  [[x-min x-max] [y-min y-max] f & {:keys [steps keysuffix]}] 
-  (let [x-increment (/ (- x-max x-min) (double (or steps plot-steps)))
+;; [xmin x-max] is the first arg, for use with -> , because it's likely
+;; that different fns will share same x coords.
+(defn f-dataset
+  [[x-min x-max] catkey catval f & {:keys [steps]}] 
+  (let [x-increment (/ (double (- x-max x-min))
+                       (or steps plot-steps))
         xs (msc/irange x-min x-max x-increment)
-        ys (map f xs)
-        xkey (keyword (str "x" (or keysuffix "")))
-        ykey (keyword (str "y" (or keysuffix "")))]
-    (-> {xkey xs, ykey ys}
+        ys (map f xs)]
+    (-> {:x xs, :y ys, catkey catval}
         tc/dataset)))
 
-(-> (fn-dataset [-4.0 1.0] [-4.0 1.0] (partial um/scaled-exp (- m/E)))
+(-> (f-dataset [-4.0 1.0] :fun :base (partial um/scaled-exp (- m/E)))
     (plotly/layer-line {:=x :x, :=y, :y}))
 
-(-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+(-> (f-dataset [-4.0 1.0] :fun :comp2
                   (msc/n-comp (partial um/scaled-exp (- m/E)) 2))
     (plotly/layer-line {:=x :x, :=y, :y}))
 
 (def all
   (tc/concat
-    (-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+    (-> (f-dataset [-4.0 1.0] :fun :base
                       (partial um/scaled-exp (- m/E))
                       :keysuffix "1"))
-    (-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+    (-> (f-dataset [-4.0 1.0] :fun :comp2
                       (msc/n-comp (partial um/scaled-exp (- m/E)) 2)
                       :keysuffix "2"))
-    (-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+    (-> (f-dataset [-4.0 1.0] :fun :comp3
                       (msc/n-comp (partial um/scaled-exp (- m/E)) 3)
                       :keysuffix "3"))))
 
 (-> all
-    (plotly/layer-line {:=x :x1, :=y, :y1})
-    (plotly/layer-line {:=x :x2, :=y, :y2})
-    (plotly/layer-line {:=x :x3, :=y, :y3}))
+    (plotly/layer-line {:=x :x, :=y, :y :=color :fun}))
+
+(comment
+         ;; old version
+         (defn fn-dataset
+           [[x-min x-max] [y-min y-max] f & {:keys [steps keysuffix]}] 
+           (let [x-increment (/ (- x-max x-min) (double (or steps plot-steps)))
+                 xs (msc/irange x-min x-max x-increment)
+                 ys (map f xs)
+                 xkey (keyword (str "x" (or keysuffix "")))
+                 ykey (keyword (str "y" (or keysuffix "")))]
+             (-> {xkey xs, ykey ys}
+                 tc/dataset)))
+
+         (-> (fn-dataset [-4.0 1.0] [-4.0 1.0] (partial um/scaled-exp (- m/E)))
+             (plotly/layer-line {:=x :x, :=y, :y}))
+
+         (-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+                         (msc/n-comp (partial um/scaled-exp (- m/E)) 2))
+             (plotly/layer-line {:=x :x, :=y, :y}))
+
+         (def all
+           (tc/concat
+             (-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+                             (partial um/scaled-exp (- m/E))
+                             :keysuffix "1"))
+             (-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+                             (msc/n-comp (partial um/scaled-exp (- m/E)) 2)
+                             :keysuffix "2"))
+             (-> (fn-dataset [-4.0 1.0] [-4.0 1.0]
+                             (msc/n-comp (partial um/scaled-exp (- m/E)) 3)
+                             :keysuffix "3"))))
+
+         (-> all
+             (plotly/layer-line {:=x :x1, :=y, :y1})
+             (plotly/layer-line {:=x :x2, :=y, :y2})
+             (plotly/layer-line {:=x :x3, :=y, :y3}))
+
+)
