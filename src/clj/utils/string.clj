3728915d4b6e4a16340dd5c5d1aaf595b42bb4ea
Marshall Abrams <marshall@logical.net>
2025-02-28 10:44:41 -0600

    Added first version function for multi-digit superscripts

--- src/clj/utils/string.clj
+++ src/clj/utils/string.clj
@@ -19,13 +19,20 @@
   "If n is a single-digit integer in [0, ..., 9], returns the
   Unicode superscript character for n.  Otherwise returns nil."
   [n]
-  (when (integer? n)
+  (when (and (integer? n) (>= n 0) (< n 10))
     (cond (= n 0) \u2070  ; superscript/subscript block
           (= n 1) \u00B9  ; Latin-1 supplement block
           (= n 2) \u00B2  ; Latin-1 supplement block
           (= n 3) \u00B3  ; Latin-1 supplement block
           (and (>= n 4) (<= n 9)) (char (+ 0x2070 n)) ; superscript/subscript block
           :else nil)))
+
+(defn u-sup-chars
+  [n]
+  (when (and (integer? n) (>= n 0) (>= n 10))
+    (loop [curr-chars [], this-n (rem n 10), next-n (quot n 10)]
+      (recur (conj curr-chars (u-sup-char this-n))))))
+
 
 (comment
   (map #(str "F" (u-sub-char %)) (concat (range 11) [0.2 -3])) ; last 3 s/b empty since nil
