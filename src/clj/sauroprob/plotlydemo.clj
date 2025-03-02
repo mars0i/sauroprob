375c2d7e35b55e7db7bbf57785b0f453427cc2c9
Marshall Abrams <marshall@logical.net>
2025-03-02 13:14:17 -0600

    Moved examples out of plotly.clj and into plotlydemo.clj

--- src/clj/sauroprob/plotlydemo.clj
+++ src/clj/sauroprob/plotlydemo.clj
@@ -1,17 +1,110 @@
 ^:kindly/hide-code
 (ns sauroprob.plotlydemo
-  (:require [scicloj.tableplot.v1.plotly :as plotly]
+  (:require [clojure.math :as m]
+            [scicloj.tableplot.v1.plotly :as plotly]
             [tablecloth.api :as tc]
-            [tablecloth.column.api :as tcc]
-            [tech.v3.datatype.datetime :as datetime]
-            [tech.v3.dataset.print :as print]
             [scicloj.kindly.v4.kind :as kind]
-            ;[clojure.string :as str]
             [scicloj.kindly.v4.api :as kindly]
             [scicloj.metamorph.ml.rdatasets :as rdatasets]
-            ;[aerial.hanami.templates :as ht]
-           ))
+            [utils.math :as um]
+            [utils.misc :as msc]
+            [sauroprob.plotly :as spl]))
 
+;; ## My dynamical system examples
+
+(def rickers
+  (let [r 2.7
+        f (um/normalized-ricker r)]
+    (tc/concat
+      (tc/dataset {:x [0 2.1], :y [0 2.1], :fun "y=x"})
+      (spl/iter-lines 0.75 8 :fun "iter" f)
+      (spl/fn2dataset [0 3] :fun "f" f)
+      (spl/fn2dataset [0 3] :fun "f^2" (msc/n-comp f 2))
+      (spl/fn2dataset [0 3] :fun "f^4" (msc/n-comp f 4)))))
+
+;; It would be simpler to embed the HTML in the vals, but this illustrates
+;; the option of adding them in the final stage.
+(-> rickers
+    (plotly/base {:=height 420 :=width 700})
+    (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+    (plotly/plot)
+    (assoc-in [:data 0 :line :dash] "dash") ; https://plotly.com/javascript/reference/scatter/#scatter-line-dash 
+    (assoc-in [:data 0 :name] "<em>y=x</em>") ; https://plotly.com/javascript/reference/scatter/#scatter-name
+    (assoc-in [:data 1 :line :dash] "dot")
+    (assoc-in [:data 2 :line :width] 3) ; default is 2.  https://plotly.com/javascript/reference/scatter/#scatter-line-width
+    (assoc-in [:data 2 :name] "<em>f(x)=xe<sup>r(1-x)</sup></em>") ; 1 shouldn't really be italicized
+    (assoc-in [:data 3 :name] "<em>f<sup>2</sup></em>")
+    (assoc-in [:data 4 :name] "<em>f<sup>4</sup></em>")
+    (spl/equalize-display-units) ; If display dimensions don't fit data, extra space in plot
+    ;(kind/pprint)
+   )
+
+(def three
+  (tc/concat
+    (tc/dataset {:x [-4 1], :y [-4 1], :fun :y=x})
+    (spl/fn2dataset [-4.0 1.0] :fun :base (partial um/scaled-exp (- m/E)))
+    (spl/fn2dataset [-4.0 1.0] :fun :comp2 (msc/n-comp (partial um/scaled-exp (- m/E)) 2))
+    (spl/fn2dataset [-4.0 1.0] :fun :comp3 (msc/n-comp (partial um/scaled-exp (- m/E)) 3))))
+
+(-> three
+    (plotly/base {:=height 600 :=width 600})
+    (plotly/layer-point {:=x :x, :=y, :y, :=color :fun, :=size :fun, :=mark-opacity 0.2,
+                        :=name "Yow"})
+    (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+    spl/equalize-display-units
+    (plotly/plot)
+    ;(kind/pprint)
+    )
+
+
+(def two
+  (let [λ1 (- (- m/E) 2.0)
+        f1 (partial um/scaled-exp λ1)
+        λ2 (- (- m/E) 3.0)
+        f2 (partial um/scaled-exp λ2)]
+    (tc/concat
+      (tc/dataset {:x [-7 0.5], :y [-7 0.5], :fun "y=x"})
+      (spl/fn2dataset [-7.0 0.5] :fun "f<sub>1</sub>(x)=λ<sub>1</sub>e<sup>x</sup>" f1)
+      (spl/fn2dataset [-7.0 0.5] :fun "f<sub>1</sub><sub>2</sub>" (msc/n-comp f1 2))
+      (spl/fn2dataset [-7.0 0.5] :fun "f<sub>2</sub>(x)=λ<sub>1</sub>e<sup>x</sup>" f2)
+      (spl/fn2dataset [-7.0 0.5] :fun "f<sub>2</sub><sub>2</sub>" (msc/n-comp f2 2))
+    )))
+
+(-> two
+    (plotly/base {:=height 600 :=width 600})
+    (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+    (spl/equalize-display-units)
+    )
+
+(let [λ (- m/E)
+      f (partial um/scaled-exp λ)]
+  (-> (tc/concat
+        (tc/dataset {:x [-7 0.5], :y [-7 0.5], :fun "<em>y</em>=<em>x</em>"})
+        (spl/fn2dataset [-7.0 0.5] :fun "f(x)=λe<sup>x</sup>" f))
+      (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+      (spl/equalize-display-units)))
+
+
+;; This illustrates a method for replacing values at the last step in order
+;; to change how they are displayed.  Using an array index seems fragile.
+(let [λ (- m/E)
+      f (partial um/scaled-exp λ)]
+  (-> (tc/concat
+        (tc/dataset {:x [-7 0.5], :y [-7 0.5], :fun "y=x"})
+        (spl/fn2dataset [-7.0 0.5] :fun "scaled-exp" f))
+      (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+      (plotly/plot)
+      ;; This works but it's fragile, and only replaces one value:
+      (assoc-in [:data 1 :name] "f(x)=λe<sup>x</sup>")
+      ;; This works but it's too complicated (cleaner with pre-defined fns):
+      ;(update :data (fn [v] (mapv
+      ;                        (fn [m] (update m :name #(if (= % "scaled-exp") "f(x)=λe<sup>x</sup>" %)))
+      ;                        v)))                    ;; use `cond` for more replacements
+      ;(kind/pprint)
+      ))
+
+
+;; ## Simple examples to explore units and margins
 
 (def data {:x [1 2 2 1 1 1.2]
            :y [1 1 3 3 2 2.5]})
@@ -38,7 +131,7 @@
     plotly/plot
 )
 
-;; Examples from Tableplot docs
+;; ## Examples based on Tableplot docs
 
 (-> (rdatasets/datasets-iris)
     (plotly/splom
@@ -61,7 +154,6 @@
                         :=mark-color "purple"}))
 
 
-;; Example from docs
 (-> (rdatasets/datasets-iris)
     (tc/random 10 {:seed 1})
     (plotly/layer-point
