e95d299c3a843e8354c1dc6e3c43a3bf23a955ba
Marshall Abrams <marshall@logical.net>
2025-03-11 14:45:58 -0500

    Added HTML examples per Timothy Pratley's comment
    https://clojurians.zulipchat.com/#narrow/channel/422115-clay-dev/topic/Commented.20kind.2Fhtml.20with.20script.20tag.3A.20strange.20behavior/near/504929010

--- src/clj/mwe2.clj
+++ src/clj/mwe2.clj
@@ -1,7 +1,19 @@
 (ns mwe2
   (:require [scicloj.kindly.v4.kind :as kind]))
 
+(kind/html "Hey")
+;; <b>Yow</b>
+
+;;Yow &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Yada
+
+(kind/html "Hi")
+
+(kind/html "<h1>Hi</h1>")
+
+;; "<h1>hello</h1>:
+
 ;; The next comment only displays '(kind/html "' and makes later lines invisible:
+
 
 ;; (kind/html "<script type=\"text/javascript\">42</script>")
 
