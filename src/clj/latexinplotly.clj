91a52db3237d718826129c31c35057699505fef4
mabrams <mabrams001@bham.rr.com>
2025-03-11 13:30:55 -0500

    Merged versions by hand

--- src/clj/latexinplotly.clj
+++ src/clj/latexinplotly.clj
@@ -7,16 +7,18 @@
             [scicloj.tableplot.v1.plotly :as plotly]
             [tablecloth.api :as tc]))
 
+;; ### Setup
+
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
 
-^:kindly/hide-code
+;; ^:kindly/hide-code
 ;; Based on https://community.plotly.com/t/latex-not-working/1735/2?u=mars0i
-;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG\">")
-;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AM_CHTML\">")
+;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG\">")
+;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AM_CHTML\">")
 
-;; ## Plots
+;; ### Plots
 
-;; This is some kind of test.
+;; Why is this section getting eaten?
 
 (-> (tc/concat
       (tc/dataset {:x [0 1], :y [0 1], :fun "$y=x^2$"})
@@ -25,13 +27,15 @@
     (plotly/plot)
     (assoc-in [:data 1 :name] "$y^2 = -x$"))
 
-;; ## $\LaTeX$
+;; ### $\LaTeX\!\!:$
 
-;; This is a test of the effects of LaTeX: $\sum_{i=0}^\infty \frac{x^i}{N}$ ! That was a sum.
+;; Here is some LaTeX, $X + Y$ which might appear on a separate line.
 
-;; $\text{This is another test of the effects of }\LaTeX\!: \sum_{i=0}^\infty \frac{x^i}{N}\text{ !}$
+;; $\text{Here is some more LaTeX: }X + Y\text{.  Did it appear on a separate line?}$
+;; If not, that shows how to embed text in LaTeX for KaTeX using `\text`.
 
 (comment
+;; This is a test of the effects of LaTeX $\sum_{i=0}^\infty \frac{x^i}{N}$ ! That was a sum.
   (require '[scicloj.clay.v2.api :as clay])
   (clay/make! {:source-path ["src/clj/latexinplotly.clj"] :format [:quarto :html]})
   (clay/make! {:source-path ["src/clj/latexinplotly.clj"] :format [:html]})
