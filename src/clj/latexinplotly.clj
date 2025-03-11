cf8e6254c76fc6f38615b3ab05da5fdafed36006
Marshall Abrams <marshall@logical.net>
2025-03-10 22:29:28 -0500

    Added new variations in commented LaTeX

--- src/clj/latexinplotly.clj
+++ src/clj/latexinplotly.clj
@@ -7,19 +7,29 @@
             [scicloj.tableplot.v1.plotly :as plotly]
             [tablecloth.api :as tc]))
 
-^:kindly/hide-code
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
 
 ^:kindly/hide-code
+;; Based on https://community.plotly.com/t/latex-not-working/1735/2?u=mars0i
+;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG\">")
+;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AM_CHTML\">")
+
+;; ## Plots
+
+;; This is some kind of test.
+
 (-> (tc/concat
       (tc/dataset {:x [0 1], :y [0 1], :fun "$y=x^2$"})
       (tc/dataset {:x [0 1], :y [1 0], :fun "y=-x"}))
     (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
     (plotly/plot)
-    (assoc-in [:data 0 :name] "y<sup>2</sup> = -x")
     (assoc-in [:data 1 :name] "$y^2 = -x$"))
 
-;; This is a test of the effects of LaTeX $\sum_{i=0}^\infty \frac{x^i}{N}$ ! That was a sum.
+;; ## $\LaTeX$
+
+;; This is a test of the effects of LaTeX: $\sum_{i=0}^\infty \frac{x^i}{N}$ ! That was a sum.
+
+;; $\text{This is another test of the effects of }\LaTeX\!: \sum_{i=0}^\infty \frac{x^i}{N}\text{ !}$
 
 (comment
   (require '[scicloj.clay.v2.api :as clay])
