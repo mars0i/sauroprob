370bc7aeca7609b3866514d64c22f19d1fcba58e
mabrams <mabrams001@bham.rr.com>
2025-03-14 23:43:18 -0500

    Abstracted the three plots in three-plots into three helper functions

--- src/clj/sauroprob/iterfreqs_fns.clj
+++ src/clj/sauroprob/iterfreqs_fns.clj
@@ -12,6 +12,58 @@
 ;; Make LaTeX work in Plotly labels:
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
 
+
+
+(defn plot-fns-with-iter-lines
+  [& {:keys [x-max  ; max x val in function plot (also max y)
+             fs     ; functions to plot
+             labels ; labels corresponding to functions
+             init-x ; initial x val for iterations--Uses the first element in fs.
+             n-iter-lines]}] ; number of iteration lines
+  (let [basef (first fs)]
+    (-> (tc/concat (sp/iter-lines init-x n-iter-lines :fun "iteration" basef)
+                   (tc/dataset {:x [0 x-max], :y [0 x-max], :fun "$y=x$"})
+                   (apply tc/concat ; maybe there's a more elegant way to do this
+                          (map (fn [label f] (sp/fn2dataset [0 x-max] :fun label f))
+                               labels fs)))
+        (plotly/base {:=height 400 :=width 550})
+        (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+        (sp/equalize-display-units) ; runs plotly/plot, which is needed for next lines
+        (sp/set-line-width 0 1.5)
+        (sp/set-line-dash 0 "dot")
+        (sp/set-line-dash 1 "dash"))))
+
+
+;; TIP:
+;; This is helper function for three-plots, but can be used alone. It's a 
+;; simple function; if more flexibility is needed for standalone use, it
+;;  would be better to write a separate function or make a plot by hand 
+;; rather than generalizing this one.
+(defn plot-iterates
+  "Plot lines connecting n iterates as y values with x representing number of
+  iterations.  iterates should be infinite or longer than n."
+  [n iterates]
+  (-> (tc/dataset {:x (range n)
+                   :y (take n iterates)})
+      (plotly/base {:=height 400 :=width 800})
+      (plotly/layer-line {:=x :x, :=y, :y})
+      plotly/plot
+      (assoc-in [:data 0 :line :width] 1)))
+
+
+;; TIP:
+;; This is helper function for three-plots, but can be used alone. It's a 
+;; simple function; if more flexibility is needed for standalone use, it
+;;  would be better to write a separate function or make a plot by hand 
+;; rather than generalizing this one.
+(defn iterates-histogram
+  "Plot the values of n iterates in a histogram. iterates should be
+  infinite or longer than n."
+  [n iterates]
+  (-> (tc/dataset {:x (take n iterates)})
+      (plotly/base {:=height 400 :=width 800})
+      (plotly/layer-histogram {:=x :x, :=histogram-nbins 200})))
+
 ;; Note the use of \text{} to display text on the same line as LaTeX without using Quarto:
 
 ;; TODO Should this be changed to allow layering of arbitrary plots in the
@@ -27,36 +79,36 @@
              init-x ; initial x val for iterations--Uses the first element in fs.
              n-iter-lines ; how many iteration lines in fn plot
              n-plot-iterates ; how many iterations in plot of iteration values
-             n-hist-iterates]}] ; how many iterations to collect in histogram
+             n-hist-iterates]
+      :as args}] ; how many iterations to collect in histogram
   (let [basef (first fs)
         iterates (iterate basef init-x)]
     (kind/fragment [(kind/md ["Plot of the function, with sample iterations beginning from " init-x ":"])
-                    (-> (tc/concat (sp/iter-lines init-x n-iter-lines :fun "iteration" basef)
-                                   (tc/dataset {:x [0 x-max], :y [0 x-max], :fun "$y=x$"})
-                                   (apply tc/concat ; maybe there's a more elegant way to do this
-                                          (map (fn [label f] (sp/fn2dataset [0 x-max] :fun label f))
-                                               labels fs))
-                                   ;(sp/fn2dataset [0 x-max] :fun "f" f)
-                                   ;(sp/fn2dataset [0 x-max] :fun "$f^2$" (msc/n-comp f 2))
-                        )
-                        (plotly/base {:=height 400 :=width 550})
-                        (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
-                        (sp/equalize-display-units) ; runs plotly/plot
-                        (sp/set-line-width 0 1.5)
-                        (sp/set-line-dash 0 "dot")
-                        (sp/set-line-dash 1 "dash"))
+                    (plot-fns-with-iter-lines args)
+                    ;(-> (tc/concat (sp/iter-lines init-x n-iter-lines :fun "iteration" basef)
+                    ;               (tc/dataset {:x [0 x-max], :y [0 x-max], :fun "$y=x$"})
+                    ;               (apply tc/concat ; maybe there's a more elegant way to do this
+                    ;                      (map (fn [label f] (sp/fn2dataset [0 x-max] :fun label f))
+                    ;                           labels fs)))
+                    ;    (plotly/base {:=height 400 :=width 550})
+                    ;    (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+                    ;    (sp/equalize-display-units) ; runs plotly/plot
+                    ;    (sp/set-line-width 0 1.5)
+                    ;    (sp/set-line-dash 0 "dot")
+                    ;    (sp/set-line-dash 1 "dash"))
 
                     (kind/md ["Plot of a sequence of values of the function beginning from " init-x ":"])
-                    (-> (tc/dataset {:x (range n-plot-iterates)
-                                     :y (take n-plot-iterates iterates)})
-                        (plotly/base {:=height 400 :=width 800})
-                        (plotly/layer-line {:=x :x, :=y, :y})
-                        plotly/plot
-                        (assoc-in [:data 0 :line :width] 1))
+                    (plot-iterates n-plot-iterates iterates)
+                    ;(-> (tc/dataset {:x (range n-plot-iterates)
+                    ;                 :y (take n-plot-iterates iterates)})
+                    ;    (plotly/base {:=height 400 :=width 800})
+                    ;    (plotly/layer-line {:=x :x, :=y, :y})
+                    ;    plotly/plot
+                    ;    (assoc-in [:data 0 :line :width] 1))
 
                     (kind/md ["Distribution of values beginning from " init-x ":"])
-                    (-> (tc/dataset {:x (take n-hist-iterates iterates)})
-                        (plotly/base {:=height 400 :=width 800})
-                        (plotly/layer-histogram {:=x :x
-                                                 :=histogram-nbins 200}))
+                    (iterates-histogram n-hist-iterates iterates)
+                    ;(-> (tc/dataset {:x (take n-hist-iterates iterates)})
+                    ;    (plotly/base {:=height 400 :=width 800})
+                    ;    (plotly/layer-histogram {:=x :x, :=histogram-nbins 200}))
                     ])))
