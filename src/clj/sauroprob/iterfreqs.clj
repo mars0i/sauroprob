55baf432aaf9d993d736a1f6122a0a4bd023da5c
mabrams <mabrams001@bham.rr.com>
2025-03-14 23:54:15 -0500

    Changed parameters of example plotting function call

--- src/clj/sauroprob/iterfreqs.clj
+++ src/clj/sauroprob/iterfreqs.clj
@@ -15,11 +15,11 @@
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
 
 (let [f (um/normalized-ricker 2.70)
-      comps [1 2 3]]
+      comps [1 2 5]]
   (fns/three-plots {:x-max 2.5
                     :fs (map (partial msc/n-comp f) comps)
                     :labels (map (fn [n] (str "$f^" n "$")) comps)
                     :init-x 0.90475
-                    :n-iter-lines 20
-                    :n-plot-iterates 400
+                    :n-iter-lines 10
+                    :n-plot-iterates 200
                     :n-hist-iterates 1000000}))
