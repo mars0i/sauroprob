6f60a95654d30a484034aa763572c2cc24546b84
Marshall Abrams <marshall@logical.net>
2025-03-15 21:48:14 -0500

    Reverted latexinplotly to version from original discussion with Timothy Pratley

--- src/clj/latexinplotly.clj
+++ src/clj/latexinplotly.clj
@@ -5,12 +5,27 @@
 (ns latexinplotly
   (:require [scicloj.kindly.v4.kind :as kind]
             [scicloj.tableplot.v1.plotly :as plotly]
-            [scicloj.metamorph.ml.rdatasets :as rdatasets]
             [tablecloth.api :as tc]))
 
-;; ## Setup
+;; ### Setup
 
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
+
+;; ^:kindly/hide-code
+;; Based on https://community.plotly.com/t/latex-not-working/1735/2?u=mars0i
+;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG\">")
+;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AM_CHTML\">")
+
+;; ### Plots
+
+;; Why is this section getting eaten?
+
+(-> (tc/concat
+      (tc/dataset {:x [0 1], :y [0 1], :fun "$y=x^2$"})
+      (tc/dataset {:x [0 1], :y [1 0], :fun "y=-x"}))
+    (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+    (plotly/plot)
+    (assoc-in [:data 1 :name] "$y^2 = -x$"))
 
 ;; ### $\LaTeX\!\!:$
 
@@ -24,91 +39,4 @@
   (require '[scicloj.clay.v2.api :as clay])
   (clay/make! {:source-path ["src/clj/latexinplotly.clj"] :format [:quarto :html]})
   (clay/make! {:source-path ["src/clj/latexinplotly.clj"] :format [:html]})
-)
-
-;; ## Plots
-
-;; (tc/head (rdatasets/datasets-mtcars))
-
-(def data (-> (tc/concat
-                (tc/dataset {:x [0 1], :y [0 1], :fun "y=x" :my-size 10})
-                (tc/dataset {:x [0 1], :y [1 0], :fun "y=-x" :my-size 50}))
-              (tc/add-columns {:line-width #(map (fn [v] (if (= v "y=x") 1 10)) (% :fun))})))
-
-(-> (tc/dataset {:x [0 1], :y [1 0] :my-size [20 30]})
-    (plotly/layer-point {:=x :x, :=y :y, :=size :my-size})
-    (plotly/layer-line {:=x :x, :=y :y, :=size :my-size}))
-
-;(-> data
-;    (plotly/layer-point {:=x :x, :=y :y, :=color :fun, :=size :my-size})
-;    (plotly/layer-line {:=x :x, :=y :y, :=color :fun, :=size 10})
-;    (plotly/plot))
-
-
-(def aplot (-> data
-               ;(tc/add-columns {:line-width #(map (fn [v] (if (= v "y=x") 1 10)) (% :fun))})
-               (plotly/layer-line {:=x :x, :=y :y, :=color :fun})
-               ;(plotly/layer-line {:=x :x, :=y :y, :=color :fun, :=mark-size :line-width, :=size-type :quantitative})
-               ;(plotly/layer-line {:=x :x, :=y :y, :=color :fun, :size [10 20]})
-               ;(plotly/layer-line {:=x :x, :=y :y, :=color :fun, :=mark-size 20})
-               ;(plotly/layer-point {:=x :x, :=y :y, :=color :fun, 
-               ;                    :=mark-size [100 200 250 150]})
-               (plotly/plot)
-               (assoc-in [:data 0 :line :dash] "dot")
-               ;(assoc-in [:data 0 :line :width] 25)
-               (assoc-in [:data 1 :name] "$y^2 = -x$")
-               ))
-
-aplot
-
-(kind/pprint aplot)
-
-(comment
-  (data :line-width)
-
-  (tc/select-rows data (fn [row] (= (:fun row) "y=x")))
-  (tc/select-rows data #(= (:fun %) "y=-x"))
-
-  (clojure.repl/pst)
-)
-
-;; This is a way to set different line widths semantically.  But it's a pain.
-(def bplot (-> data
-               (tc/select-rows #(= (:fun %) "y=x"))
-               (plotly/layer-line {:=x :x, :=y :y, :=color :fun :=mark-size 10})
-               (plotly/plot)))
-
-(def cplot (-> data
-               (tc/select-rows #(= (:fun %) "y=-x"))
-               (plotly/layer-line {:=x :x, :=y :y, :=color :fun :=mark-size 20})
-               (plotly/plot)))
-
-;; plotly/plot needed in next line to add kind metadata:
-(def dplot (plotly/plot
-             {:data (vec (concat (:data bplot) (:data cplot)))
-              :layout (:layout bplot)}))
-;; Is there some way to use merge-with?
-
-
-dplot
-
-;; This eats things that come after it:
-
-;; ^:kindly/hide-code
-;; Based on https://community.plotly.com/t/latex-not-working/1735/2?u=mars0i
-;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG\">")
-;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AM_CHTML\">")
-
-
-
-(comment
-  (tc/add-columns data {:V1 #(map inc (% :x))
-                        :V5 #(map (comp keyword str) (% :y))
-                        :V6 11
-                        :lw #(map (fn [v] (if (= v "y=x") 25 42)) (% :fun))})
-
-  (tc/add-columns data {:lw #(map (fn [v] (if (= v "y=x") 25 42)) (% :fun))})
-
-  ;; fails--the map brackets are necessary:
-  (tc/add-columns data :lw #(map (fn [v] (if (= v "y=x") 25 42)) (% :fun)))
 )
