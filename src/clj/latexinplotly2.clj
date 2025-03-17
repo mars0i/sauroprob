992d3183a8e715a864703ef021889e0480b47500
Marshall Abrams <marshall@logical.net>
2025-03-17 10:49:22 -0500

    Made latexinplotly2.clj into a proper MWE

--- src/clj/latexinplotly2.clj
+++ src/clj/latexinplotly2.clj
@@ -1,56 +1,41 @@
 ^:kindly/hide-code
 ;; (Based on https://clojurians.zulipchat.com/#narrow/channel/422115-clay-dev/topic/.E2.9C.94.20LaTeX.20.20in.20TMD.20fields.20into.20Plotly.20legends.3F/near/504227110)
 
-^:kindly/hide-code
 (ns latexinplotly2
-  (:require [scicloj.clay.v2.api :as clay] ; only needed for make!
-            [scicloj.kindly.v4.kind :as kind]
+  (:require [scicloj.kindly.v4.kind :as kind]
             [scicloj.tableplot.v1.plotly :as plotly]
             [tablecloth.api :as tc]))
 
 
-;; # setup
+;; ## Setup
 
-^:kindly/hide-code
 (comment
+  (require '[scicloj.clay.v2.api :as clay])
+
+  (clay/make! {:source-path ["src/clj/latexinplotly2.clj"]
+               :format [:html]})
+
+  (clay/make! {:source-path ["src/clj/latexinplotly2.clj"]
+               :format [:quarto :html]})
+
   (clay/make! {:source-path ["src/clj/latexinplotly2.clj"]
                :format [:quarto :html]
-               ;; Adding this line allows Quarto processing to include Plotly plots
-               ;; while also making LaTeX inline in comments:
-               :quarto {:format {:html {:html-math-method :katex}}}
-               ;; However, uncommening the mathjax line to cause Plotly to
-               ;; process LaTeX in labels 
-               })
+               ;; This is the extra line that allows LaTeX in Plotly with Quarto:
+               :quarto {:format {:html {:html-math-method :katex}}}})
 )
 
-;; This makes LaTeX in Plotly labels work (without Quarto; Plotly messes up plots with Quarto):
+;; This makes LaTeX in Plotly labels work:
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
+;; Note this apparently must be an instance of MathJax 2.  MathJax v3 doesn't
+;; seem to work, although some documentation suggests it should.
 
-;; Note this apparently must be an instance of MathJax 2. v3 doesb't
-;; seem to work (although some documentation suggests it should).
-
-;; Did $\LaTeX$ appear on a separate line here?  That's what the default
-;; `kind/tex` use of KaTeX does.  Quarto, on the other hand, doesn't 
-;; break lines around LaTeX in comments.
-
-;; # a plot
+;; ## A plot
 
 (-> (tc/concat
-      (tc/dataset {:x [0 1], :y [0 1], :fun "$y=x^2$"})
-      (tc/dataset {:x [0 1], :y [1 0], :fun "y=-x"}))
-    (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
-    (plotly/plot)
-    (assoc-in [:data 1 :name] "$y^2 = -x$"))
+      (tc/dataset {:x [0 1], :y [0 1], :fun "$y_1=x_1$"})
+      (tc/dataset {:x [0 1], :y [1 0], :fun "$y^2 = -z^3$"}))
+    (plotly/layer-line {:=x :x, :=y, :y :=color :fun}))
 
+;; ## LaTeX-in-comments example
 
-;; # stuff
-
-(comment
-  (clay/make! {:source-path ["src/clj/latexinplotly2.clj"] :format [:quarto :html]})
-  (clay/make! {:source-path ["src/clj/latexinplotly2.clj"] :format [:html]})
-)
-
-;; ^:kindly/hide-code
-;; Based on https://community.plotly.com/t/latex-not-working/1735/2?u=mars0i
-;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG\">")
-;;(kind/html "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AM_CHTML\">")
+;; Did $\sum_{j=0}^\infty \frac{1}{2^j}$ appear on a separate line here? (That's what happens without Quarto.)
