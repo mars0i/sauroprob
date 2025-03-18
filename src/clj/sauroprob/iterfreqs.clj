72c3b616076d28e55faf2f9e4d35812f3518ed0d
Marshall Abrams <marshall@logical.net>
2025-03-18 12:42:47 -0500

    Added explorations of Ricker iterations

--- src/clj/sauroprob/iterfreqs.clj
+++ src/clj/sauroprob/iterfreqs.clj
@@ -14,12 +14,96 @@
 ;; Make LaTeX work in Plotly labels:
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
 
-(let [f (um/normalized-ricker 3.5)
+um/ricker-chaos-min
+
+;; This is below the chaos min:
+(let [f (um/normalized-ricker 2.6)
       comps [1]]
-  (fns/three-plots {:x-max 2.5
+  (fns/three-plots {:x-max 4
                     :fs (map (partial msc/n-comp f) comps)
                     :labels (map (fn [n] (str "$f^" n "$")) comps)
                     :init-x 0.901
+                    :n-cobweb 30
+                    :n-plot-iterates 400
+                    :n-hist-iterates 1000000}))
+
+;; Well, this seems pretty periodic, though it's (just above the chaos min):
+(let [f (um/normalized-ricker 2.6925)
+      comps [1]]
+  (fns/three-plots {:x-max 4
+                    :fs (map (partial msc/n-comp f) comps)
+                    :labels (map (fn [n] (str "$f^" n "$")) comps)
+                    :init-x 0.901
+                    :n-cobweb 30
+                    :n-plot-iterates 400
+                    :n-hist-iterates 1000000}))
+
+;; Still kinda periodic, but maybe with intervening noise:
+(let [f (um/normalized-ricker 2.7)
+      comps [1]]
+  (fns/three-plots {:x-max 4
+                    :fs (map (partial msc/n-comp f) comps)
+                    :labels (map (fn [n] (str "$f^" n "$")) comps)
+                    :init-x 0.901
+                    :n-cobweb 30
+                    :n-plot-iterates 400
+                    :n-hist-iterates 1000000}))
+
+;; Better:
+(let [f (um/normalized-ricker 3.0)
+      comps [1]]
+  (fns/three-plots {:x-max 4
+                    :fs (map (partial msc/n-comp f) comps)
+                    :labels (map (fn [n] (str "$f^" n "$")) comps)
+                    :init-x 0.901
+                    :n-cobweb 30
+                    :n-plot-iterates 400
+                    :n-hist-iterates 1000000}))
+
+;; Let's try a different init x:
+(let [f (um/normalized-ricker 3.0)
+      comps [1]]
+  (fns/three-plots {:x-max 4
+                    :fs (map (partial msc/n-comp f) comps)
+                    :labels (map (fn [n] (str "$f^" n "$")) comps)
+                    :init-x 0.4
+                    :n-cobweb 30
+                    :n-plot-iterates 400
+                    :n-hist-iterates 1000000}))
+
+
+
+;; Again:
+(let [f (um/normalized-ricker 3.0)
+      comps [1]]
+  (fns/three-plots {:x-max 4
+                    :fs (map (partial msc/n-comp f) comps)
+                    :labels (map (fn [n] (str "$f^" n "$")) comps)
+                    :init-x 0.35
+                    :n-cobweb 30
+                    :n-plot-iterates 400
+                    :n-hist-iterates 1000000}))
+
+;; Again again:
+(let [f (um/normalized-ricker 3.0)
+      comps [1]]
+  (fns/three-plots {:x-max 4
+                    :fs (map (partial msc/n-comp f) comps)
+                    :labels (map (fn [n] (str "$f^" n "$")) comps)
+                    :init-x 0.75
+                    :n-cobweb 30
+                    :n-plot-iterates 400
+                    :n-hist-iterates 1000000}))
+
+
+
+;; This init is the fixed point:
+(let [f (um/normalized-ricker 3.0)
+      comps [1]]
+  (fns/three-plots {:x-max 4
+                    :fs (map (partial msc/n-comp f) comps)
+                    :labels (map (fn [n] (str "$f^" n "$")) comps)
+                    :init-x 1
                     :n-cobweb 30
                     :n-plot-iterates 400
                     :n-hist-iterates 1000000}))
