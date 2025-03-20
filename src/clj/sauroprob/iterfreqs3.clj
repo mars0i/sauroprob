3d506e160d879c79dd8adaa3cbc70b7be42193f3
mabrams <mabrams001@bham.rr.com>
2025-03-19 22:58:06 -0500

    Removed old development/experimental cdf-plotting code

--- src/clj/sauroprob/iterfreqs3.clj
+++ src/clj/sauroprob/iterfreqs3.clj
@@ -24,25 +24,6 @@
                     :n-seq-iterates 400
                     :n-dist-iterates n-iterates}))
 
-^:kindly/hide-code
-(def l4iterates (iterate um/logistic-4 0.901))
-^:kindly/hide-code
-(def l4dist (fr/distribution :real-discrete-distribution {:data (take n-iterates l4iterates)}))
-^:kindly/hide-code
-(def l4cdf (partial fr/cdf l4dist))
-
-^:kindly/hide-code
-(def l4xs (range 0 1 0.001))
-^:kindly/hide-code
-(let [l4cdf-ys (map l4cdf l4xs)]
-  (-> (tc/dataset {:x l4xs
-                   :y l4cdf-ys})
-      (plotly/layer-line {:=x :x, :=y, :y})
-      plotly/plot
-      (assoc-in [:data 0 :line :width] 0.75)))
-
-(fns/plot-cdf 1 (take 100000 l4iterates))
-
 ;; Let's try a different init x:
 (let [f um/logistic-4
       comps [1]]
