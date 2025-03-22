36554f4e1ff39fb95a277506fad082f37defdecc
Marshall Abrams <marshall@logical.net>
2025-03-22 15:33:10 -0500

    Explored adding labels

--- src/clj/plotlysubplots.clj
+++ src/clj/plotlysubplots.clj
@@ -1,3 +1,4 @@
+^:kindly/hide-code
 (ns plotlysubplots
   (:require [scicloj.tableplot.v1.plotly :as plotly]
             [scicloj.kindly.v4.kind :as kind]
@@ -26,6 +27,7 @@
 (def init-x 0.3)
 (def iterates (iterate f init-x))
 
+^:kindly/hide-code
 (def cobthing
   (let [comps [1]]
     (-> (fns/plot-fns-with-cobweb {:x-max 3
@@ -36,6 +38,7 @@
                                    :n-seq-iterates 400
                                    :n-dist-iterates n-hist-iterates}))))
 
+^:kindly/hide-code
 ;; A plot with three traces in cobthing, and three other traces
 (def combothing
   (plotly/plot ; add meta
@@ -47,6 +50,7 @@
                             (fns/plot-iterates-histogram (take n-hist-iterates iterates))
                             (fns/plot-cdf 3 (take n-hist-iterates iterates))]))}))
 
+^:kindly/hide-code
 ;; Split the traces into a grid of plots:
 ;; The values in in xn yn determines the order in the grid.
 (def multiplotthing
@@ -86,84 +90,15 @@
       (assoc-in [:layout :xaxis2] {:anchor "y2", :domain [0.3, 1.0]}) ; why are axis bars shifted left?
       (assoc-in [:layout :xaxis4] {:anchor "y4", :domain [0.3, 1.0]})
 
+      ;(assoc-in [:layout :annotations]
+      ;          [{:x 1
+      ;            :y 0
+      ;            :text "Values from iterating the function"
+      ;            :showarrow false}])
+
       ;(assoc-in [:data 0 :line :width] 0.5) ; s/b the diagonal y=x line
   ))
 
 multiplotthing
 
-;; ---
-
-
-^:kindly/hide-code
-(comment
-(def data3 (tc/concat
-             ;; These are going to be four distinct "traces" in the plotly config:
-             (tc/dataset {:x [0], :y [0], :fun "line1" :my-size 20})
-             (tc/dataset {:x [1], :y [1], :fun "line1" :my-size 60})
-             (tc/dataset {:x [0], :y [1], :fun "line2" :my-size 200})
-             (tc/dataset {:x [1], :y [0], :fun "line2" :my-size 600})))
-
-(def uniplot (-> data3
-                 (plotly/layer-point {:=x :x, :=y :y, :=size :my-size, :=color :fun})
-                 (plotly/layer-line {:=x :x, :=y :y, :=size :my-size, :=color :fun})
-                 (plotly/plot)))
-
-uniplot
-
-(def multiplot (-> uniplot
-                   ;; Do nothing to traces 0 and 1.  They will be part of the first subplot.
-                   ;; But if you name something "x1" and "y1", it will go there too.
-                   ;; By adding the :xaxis and :yaxis keywords, and giving them one of the
-                   ;; official names "x2" and "y2", they go on a second subplot.
-                   (assoc-in [:data 2 :xaxis] "x2")
-                   (assoc-in [:data 2 :yaxis] "y2")
-                   ;; And here's the third subplot:
-                   (assoc-in [:data 3 :xaxis] "x3")
-                   (assoc-in [:data 3 :yaxis] "y3")
-                   ;; https://plotly.com/javascript/reference/layout/#layout-grid-pattern
-                   ;; `:pattern "independent"` seems to be necessary to allow plotly to
-                   ;; work out the placement on its own.  The alternative, "coupled",
-                   ;; seems to require more configuration info.  That's a guess.
-                   (assoc-in [:layout :grid] {:rows 2, :columns 2, :pattern "independent"})
-                   ;; Since there are only three subplots, the fourth cell will be empty.
-                   ;(kind/pprint)
-                   ))
-
-
-multiplot
-
-(kind/pprint uniplot)
-
-(kind/pprint multiplot)
-
-;; ---
-
-(def uniricker
-  (let [f (um/normalized-ricker 3.0)
-        comps [1 4]]
-    (-> (fns/plot-fns-with-cobweb {:x-max 4
-                                   :fs (map (partial msc/n-comp f) comps)
-                                   :labels (map (fn [n] (str "$f^" n "$")) comps)
-                                   :init-x 2.1
-                                   :n-cobweb 10
-                                   :n-seq-iterates 400
-                                   :n-dist-iterates n-hist-iterates}))))
-
-(def multiricker
-  (-> uniricker
-      (assoc-in [:data 0 :xaxis] "x1") ; default--could be left out
-      (assoc-in [:data 0 :yaxis] "y1")
-      (assoc-in [:data 1 :xaxis] "x2")
-      (assoc-in [:data 1 :yaxis] "y2")
-      (assoc-in [:data 2 :xaxis] "x1")
-      (assoc-in [:data 2 :yaxis] "y1")
-      (assoc-in [:data 3 :xaxis] "x6")
-      (assoc-in [:data 3 :yaxis] "y6")
-      (assoc-in [:layout :grid] {:rows 3, :columns 2, :pattern "independent"})))
-
-multiricker
-
-(kind/pprint multiricker)
-)
-
-;; ---
+;(kind/pprint multiplotthing)
