dd85c9b5b656f5d0124daf2908aa97088a4be619
Marshall Abrams <marshall@logical.net>
2025-03-27 09:43:20 -0500

    Added K-S test examples

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -14,10 +14,10 @@
 
 (kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
 
-fs/ks-test-two-samples
 
 (def logisticvals1 (iterate um/logistic-4 0.14))
-(def logisticvals2 (iterate um/logistic-4 0.17))
+(def logisticvals2 (iterate um/logistic-4 0.16))
+(def logisticvals3 (iterate um/logistic-4 0.41))
 
 
 (map #(ifn/plots-grid {:x-max 1.0 
@@ -26,5 +26,34 @@ fs/ks-test-two-samples
                   :init-x %
                   :n-cobweb 14
                   :n-seq-iterates 300
-                  :n-dist-iterates 1000})
+                  :n-dist-iterates 10000})
      [0.14 0.16 0.41])
+
+;; When I compared 0.14 and 0.17 init values, 
+;; the p-values below were all near 0.1, give or take, which is not very good.
+;; The K-S statistic varies between about 1.8 and around 5.5.  What's that
+;; about?  i.e. it's changing as I extend the very same sequence.
+
+;; Below I compare 0.14 and 0.16.  Still large p-values, and fluctuating
+;; K-S statistics as I extend the sequence:
+
+(let [N 1000]
+  (fs/ks-test-two-samples 
+    (take N logisticvals1)
+    (take N logisticvals2)))
+
+(let [N 10000]
+  (fs/ks-test-two-samples 
+    (take N logisticvals1)
+    (take N logisticvals2)))
+
+(let [N 100000]
+  (fs/ks-test-two-samples 
+    (take N logisticvals1)
+    (take N logisticvals2)))
+
+(let [N 1000000]
+  (fs/ks-test-two-samples 
+    (take N logisticvals1)
+    (take N logisticvals2)))
+
