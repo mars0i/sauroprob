ab5adec1d64563eb7bcfc14d3f1ae6df99db43b1
Marshall Abrams <marshall@logical.net>
2025-04-24 16:22:20 -0500

    Reformatted code, commented out require

--- src/clj/sauroprob/iterfreqs2.clj
+++ src/clj/sauroprob/iterfreqs2.clj
@@ -5,9 +5,9 @@
             [scicloj.kindly.v4.kind :as kind]
             [scicloj.kindly.v4.api :as kindly]
             [tablecloth.api :as tc]
+            ;[scicloj.metamorph.ml.rdatasets :as rdatasets]
             [utils.misc :as msc]
             [utils.math :as um]
-          [scicloj.metamorph.ml.rdatasets :as rdatasets]
             [sauroprob.plotly :as sp]
             [sauroprob.iterfreqs-fns :as fns]))
 
@@ -19,7 +19,7 @@
 
 (def iters (take 10000 (iterate (um/normalized-ricker 3) 2.1)))
 
-(def histplot (fns/iterates-histogram iters))
+(def histplot (fns/plot-iterates-histogram iters))
 (def cdfplot (-> (fns/plot-cdf 3.5 iters)
                  (assoc-in [:layout :title] "CDF, Baby!")
                  ))
@@ -39,22 +39,22 @@
 (let [f (um/normalized-ricker 3.0)
       comps [1]]
   (fns/plots {:x-max 4
-                    :fs (map (partial msc/n-comp f) comps)
-                    :labels (map (fn [n] (str "f^" n)) comps) ; removed LaTeX see mwe5.clj
-                    :init-x 2.1
-                    :n-cobweb 10
-                    :n-seq-iterates 400
-                    :n-dist-iterates hist-iterates}))
+              :fs (map (partial msc/n-comp f) comps)
+              :labels (map (fn [n] (str "f^" n)) comps) ; removed LaTeX see mwe5.clj
+              :init-x 2.1
+              :n-cobweb 10
+              :n-seq-iterates 400
+              :n-dist-iterates hist-iterates}))
 
 ;; Let's try a different init x:
 (let [f (um/normalized-ricker 3.0)
       comps [1]]
   (fns/plots {:x-max 4
-                    :fs (map (partial msc/n-comp f) comps)
-                    :labels (map (fn [n] (str "f^" n)) comps) ; removed LaTeX seem mwe5.clj
-                    :init-x 0.001
-                    :n-cobweb 10
-                    :n-seq-iterates 400
-                    :n-dist-iterates hist-iterates}))
+              :fs (map (partial msc/n-comp f) comps)
+              :labels (map (fn [n] (str "f^" n)) comps) ; removed LaTeX seem mwe5.clj
+              :init-x 0.001
+              :n-cobweb 10
+              :n-seq-iterates 400
+              :n-dist-iterates hist-iterates}))
 
 
