421e71b988b6939f575d6c6c063102fb57fdc307
Marshall Abrams <marshall@logical.net>
2025-05-01 12:48:36 -0500

    More experiments with large N

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -17,24 +17,17 @@
 ;; (Note that you'd think that floor could go immediately to a fixed point 
 ;; of 0 or K, but it depends on what the second value is.)
 
-(comment
-  (let [K 1000
-        r 3.0143
-        f (um/ricker K r)
-        ceiled-f (um/ceiled f)
-        floored-f (um/floored f)
-        x-max (* K 2)
-        init-x 0.901]
-    (take 10 (iterate floored-f init-x)))
-  )
-
-(let [K3   1000
-      K4  10000
-      K5 100000
+(let [K3     1000
+      K4    10000
+      K5   100000 ; 100K
+      K6  1000000 ; 1M
+      K7 10000000 ; 10M
       r 3.0143
       ceiled-f-3 (um/ceiled (um/ricker K3 r))
       ceiled-f-4 (um/ceiled (um/ricker K4 r))
       ceiled-f-5 (um/ceiled (um/ricker K5 r))
+      ceiled-f-6 (um/ceiled (um/ricker K6 r))
+      ceiled-f-7 (um/ceiled (um/ricker K7 r))
       x-max-3 (* K3 2)
       x-max-4 (* K4 2)
       x-max-5 (* K5 2)
@@ -43,7 +36,7 @@
       n-seq-iterates 200
       n-dist-iterates 2000
       comps [1]]
-  (kind/fragment [
+  (kind/fragment [(comment
                   (fns/plots-grid {:x-max x-max-3
                                    :fs (map (partial msc/n-comp ceiled-f-3) comps)
                                    :labels (map (fn [n] (str "ceiled^" n)) comps)
@@ -51,59 +44,28 @@
                                    :n-cobweb n-cobweb
                                    :n-seq-iterates n-seq-iterates
                                    :n-dist-iterates n-dist-iterates})
-                  (fns/plots-grid {:x-max x-max-4
-                                   :fs (map (partial msc/n-comp ceiled-f-4) comps)
-                                   :labels (map (fn [n] (str "ceiled^" n)) comps)
-                                   :init-x init-x
-                                   :n-cobweb n-cobweb
-                                   :n-seq-iterates n-seq-iterates
-                                   :n-dist-iterates n-dist-iterates})
-                  (comment
-                  (fns/plots-grid {:x-max x-max-5
-                                   :fs (map (partial msc/n-comp ceiled-f-5) comps)
-                                   :labels (map (fn [n] (str "ceiled^" n)) comps)
-                                   :init-x init-x
-                                   :n-cobweb n-cobweb
-                                   :n-seq-iterates n-seq-iterates
-                                   :n-dist-iterates n-dist-iterates})
                   )
-                  ]))
-
-
-(comment
-  (let [K 1000
-        r 3.0143
-        f (um/ricker K r)
-        ceiled-f (um/ceiled f)
-        floored-f (um/floored f)
-        x-max (* K 2)
-        init-x 0.901
-        n-cobweb 30
-        n-seq-iterates 200
-        n-dist-iterates 10000
-        comps [1]]
-    (kind/fragment [
-                    (fns/plots-grid {:x-max x-max
-                                     :fs (map (partial msc/n-comp f) comps)
-                                     :labels (map (fn [n] (str "ricker^" n)) comps)
-                                     :init-x init-x
-                                     :n-cobweb n-cobweb
-                                     :n-seq-iterates n-seq-iterates
-                                     :n-dist-iterates n-dist-iterates})
-                    (fns/plots-grid {:x-max x-max
-                                     :fs (map (partial msc/n-comp ceiled-f) comps)
+                  (fns/plot-iterates-histogram (take n-dist-iterates (iterate ceiled-f-7 init-x)))
+                  ;; This makes Java go kerfluey, even with f-3:
+                  ;(fns/plot-cdf (take n-dist-iterates (iterate ceiled-f-3 init-x))
+                  ;              x-max-4 0)
+                  (comment
+                    (fns/plots-grid {:x-max x-max-4
+                                     :fs (map (partial msc/n-comp ceiled-f-4) comps)
                                      :labels (map (fn [n] (str "ceiled^" n)) comps)
                                      :init-x init-x
                                      :n-cobweb n-cobweb
                                      :n-seq-iterates n-seq-iterates
                                      :n-dist-iterates n-dist-iterates})
-                    (fns/plots-grid {:x-max x-max
-                                     :fs (map (partial msc/n-comp floored-f) comps)
-                                     :labels (map (fn [n] (str "floored^" n)) comps)
+                  )
+                  (comment
+                    (fns/plots-grid {:x-max x-max-5
+                                     :fs (map (partial msc/n-comp ceiled-f-5) comps)
+                                     :labels (map (fn [n] (str "ceiled^" n)) comps)
                                      :init-x init-x
                                      :n-cobweb n-cobweb
                                      :n-seq-iterates n-seq-iterates
                                      :n-dist-iterates n-dist-iterates})
-                    ]))
-  )
+                  )
+                  ]))
 
