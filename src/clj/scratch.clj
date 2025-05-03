b72e683a20c1d3850f46aba68bc024d04e64ee88
Marshall Abrams <marshall@logical.net>
2025-05-03 16:08:46 -0500

    Experimenting with cdf bug

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -17,7 +17,9 @@
 ;; (Note that you'd think that floor could go immediately to a fixed point 
 ;; of 0 or K, but it depends on what the second value is.)
 
-(let [K3     1000
+
+(msc/multidef 
+     [K3     1000
       K4    10000
       K5   100000 ; 100K
       K6  1000000 ; 1M
@@ -34,21 +36,22 @@
       init-x 0.901
       n-cobweb 30
       n-seq-iterates 200
-      n-dist-iterates 2000
-      comps [1]]
+      n-dist-iterates 1000
+      dist-iterates-3 (take n-dist-iterates (iterate ceiled-f-3 init-x))
+      comps [1]])
+
   (kind/fragment [(comment
-                  (fns/plots-grid {:x-max x-max-3
-                                   :fs (map (partial msc/n-comp ceiled-f-3) comps)
-                                   :labels (map (fn [n] (str "ceiled^" n)) comps)
-                                   :init-x init-x
-                                   :n-cobweb n-cobweb
-                                   :n-seq-iterates n-seq-iterates
-                                   :n-dist-iterates n-dist-iterates})
+                    (fns/plots-grid {:x-max x-max-3
+                                     :fs (map (partial msc/n-comp ceiled-f-3) comps)
+                                     :labels (map (fn [n] (str "ceiled^" n)) comps)
+                                     :init-x init-x
+                                     :n-cobweb n-cobweb
+                                     :n-seq-iterates n-seq-iterates
+                                     :n-dist-iterates n-dist-iterates})
                   )
-                  (fns/plot-iterates-histogram (take n-dist-iterates (iterate ceiled-f-7 init-x)))
-                  ;; This makes Java go kerfluey, even with f-3:
-                  ;(fns/plot-cdf (take n-dist-iterates (iterate ceiled-f-3 init-x))
-                  ;              x-max-4 0)
+                  ;(fns/plot-iterates-histogram (take n-dist-iterates (iterate ceiled-f-7 init-x)))
+                  ;; This makes Java go kerflooey, even with f-3:
+                 (fns/plot-cdf dist-iterates-3 x-max-4 0)
                   (comment
                     (fns/plots-grid {:x-max x-max-4
                                      :fs (map (partial msc/n-comp ceiled-f-4) comps)
@@ -67,5 +70,5 @@
                                      :n-seq-iterates n-seq-iterates
                                      :n-dist-iterates n-dist-iterates})
                   )
-                  ]))
+                  ])
 
