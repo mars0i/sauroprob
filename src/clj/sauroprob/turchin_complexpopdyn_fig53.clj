c52aed0c404efad1d5a11ce7e096ccf9477ad375
Marshall Abrams <mabrams001@bham.rr.com>
2025-08-27 23:03:22 -0500

    Revised and generalized

--- src/clj/sauroprob/turchin_complexpopdyn_fig53.clj
+++ src/clj/sauroprob/turchin_complexpopdyn_fig53.clj
@@ -20,21 +20,37 @@
 ;; Side effects: Shifts labels in plotly, makes Clay not return to same position.
 ;(kind/hiccup [:script {:src "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML"}])
 
-^:kindly/hide-code
-;; I tried to move this later and forward declare it, but that doesn't seem to work.
 (defn turchin53
-  "Returns a version of the function like the one that Turchin 2003 uses to
-  generate figure 5.3.  jog-slope should usually be a small negative
+  "A modified Ricker function like the one that Turchin 2003 uses
+  to generate figure 5.3.  jog-slope should usually be a small negative
+  number.  This will be the slope of the 'jog', the region in which the
+  Ricker function is replaced with a jog function.  jog-min and jog-max
+  should be numbers near 1 s.t. 1 falls within (jog-min, jog-max). This is
+  the interval in which the Ricker function will be replaced by the linear
+  'jog' function. r is the parameter of a normalized Ricker map."
+  [jog-fn jog-min jog-max r x]
+  (if (and (> x jog-min)
+           (< x jog-max))
+    (jog-fn x)
+    (um/normalized-ricker r x)))
+
+(defn linear-jog
+  [jog-slope]
+  (fn [x]
+    (+ (* jog-slope x)
+       (- 1 jog-slope))))
+
+;; I tried to move this later and forward declare it, but that doesn't seem to work.
+(defn turchin53-linear
+  "A modified Ricker function like the one that Turchin 2003 uses
+  to generate figure 5.3.  jog-slope should usually be a small negative
   number.  This will be the slope of the 'jog', the region in which the
   Ricker function is replaced with a linear function.  jog-min and jog-max
   should be numbers near 1 s.t. 1 falls within (jog-min, jog-max). This is
   the interval in which the Ricker function will be replaced by the linear
   'jog' function. r is the parameter of a normalized Ricker map."
-  [jog-min jog-max jog-slope r x]
-  (if (and (> x jog-min)
-           (< x jog-max))
-    (+ (* jog-slope x) (+ 1 (- jog-slope))) ; alter the intercept so line goes through (1,1)
-    (um/normalized-ricker r x)))
+  [jog-slope jog-min jog-max r x]
+  (turchin53 (linear-jog jog-slope) jog-min jog-max r x))
 
 (def jog-width 0.1)
 (def half-width (/ jog-width 2))
@@ -56,6 +72,7 @@
                     ;:init-x 1.01 ; works well for jog-width 0.01
                     :init-x 1.8075 ; works well for jog-width 0.1
                     :n-cobweb 30
+                    :n-dist-iterates 280
                     :n-seq-iterates 280
                     ;:intro-label-md (str "$r=" 3.5 ":$") 
                    })
@@ -63,31 +80,35 @@
 
 ;; This is a Ricker function with a "jog" near (1,1).  It's like a Ricker 
 ;; function with r=3.5, but near 1, it's linear with a small negative slope.
-(let [f (partial turchin53 jog-min jog-max -0.5 3.5)
+(let [f (partial turchin53-linear -0.5 jog-min jog-max 3.5)
       comps [1]]
   (fns/plots-grid (merge 
+                    common-params
                     {:fs (map (partial msc/n-comp f) comps)
                      :labels (map (fn [n] (str "$f^" n "$")) comps)
-                     :n-dist-iterates 280}
-                    common-params)))
+                    })))
 
 ;; This is a Ricker function with a "jog" near (1,1) and added noise.
 ;; Without noise, it would be like a Ricker function with r=3.5, but near 1, 
 ;; linear with a small negative slope.
 (let [f (partial noisy-fn rng 0.89 1.11 ; (- 1 (* 1.5 half-width)) (+ 1 (* 1.5 half-width))
-                 (partial turchin53 jog-min jog-max -0.5 3.5))
+                 (partial turchin53-linear -0.5 jog-min jog-max 3.5)
+                 )
       comps [1]]
   (fns/plots-grid (merge 
+                    common-params
                     {:fs (map (partial msc/n-comp f) comps)
                      :labels (map (fn [n] (str "$f^" n "$")) comps)
-                     :n-dist-iterates 280}
-                    common-params)))
+                     :n-cobweb 100
+                    })))
+
+
 
 ;; This is the original Ricker function without the modification near 1.
-(let [f (partial um/normalized-ricker 3.5)
+(let [f (partial um/normalized-ricker 1.25)
       comps [1]]
   (fns/plots-grid (merge 
+                    common-params
                     {:fs (map (partial msc/n-comp f) comps)
                      :labels (map (fn [n] (str "$f^" n "$")) comps)
-                     :n-dist-iterates 5000}
-                    common-params)))
+                     :n-dist-iterates 5000})))
