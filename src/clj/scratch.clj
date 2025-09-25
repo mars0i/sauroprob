dde7e7fe06a62a172263e1a077ec9a730e433063
Marshall Abrams <mabrams001@bham.rr.com>
2025-09-25 13:29:43 -0500

    Functions for Seger and Brockman 1987 on bet hedging

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -1,6 +1,7 @@
 ^:kindly/hide-code
 (ns scratch
-  (:require [scicloj.kindly.v4.kind :as kind]
+  (:require [clojure.math :as m]
+            [scicloj.kindly.v4.kind :as kind]
             [scicloj.tableplot.v1.plotly :as plotly]
             [fastmath.random :as fr]
             [fastmath.stats :as fs]
@@ -12,3 +13,47 @@
             [sauroprob.iterfreqs-fns :as fns]
             [sauroprob.plotly :as sp]
             [sauroprob.iterfreqs-fns :as ifn]))
+
+
+;; Not really about chaos
+;; From Seger and Brockman 1987 p. 192 on bet hedging
+
+;; Mixture of the two specialist strategies:
+
+;(clojure.repl/dir clojure.math)
+
+(defn mixed-expectation
+  [p]
+  (let [q (- 1 p)
+        dry-specialist-in-dry p
+        dry-specialist-in-wet (* q 0.6)
+        wet-specialist-in-wet q
+        wet-specialist-in-dry (* q 0.58)
+        wet-sum (+ dry-specialist-in-wet wet-specialist-in-wet)
+        dry-sum (+ wet-specialist-in-dry dry-specialist-in-dry)]
+    (* 1/2 (+ wet-sum dry-sum))))
+
+(defn mixed-variance
+  [p]
+  (let [q (- 1 p)
+        dry-specialist-in-dry p
+        dry-specialist-in-wet (* q 0.6)
+        wet-specialist-in-wet q
+        wet-specialist-in-dry (* q 0.58)
+        wet-sum (+ dry-specialist-in-wet wet-specialist-in-wet)
+        dry-sum (+ wet-specialist-in-dry dry-specialist-in-dry)
+        wet-sum-sq (* wet-sum wet-sum)
+        dry-sum-sq (* dry-sum dry-sum)]
+    (- (* 1/2 (+ wet-sum-sq dry-sum-sq))
+       (mixed-expectation p))))
+
+(defn mixed-geom
+  [p]
+  (let [q (- 1 p)
+        dry-specialist-in-dry p
+        dry-specialist-in-wet (* q 0.6)
+        wet-specialist-in-wet q
+        wet-specialist-in-dry (* q 0.58)
+        wet-sum (+ dry-specialist-in-wet wet-specialist-in-wet)
+        dry-sum (+ wet-specialist-in-dry dry-specialist-in-dry)]
+    (m/sqrt (* wet-sum dry-sum))))
