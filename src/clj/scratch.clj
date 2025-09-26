e9b759d6b601a22f8a3f134506f287251c603740
Marshall Abrams <mabrams001@bham.rr.com>
2025-09-26 13:44:15 -0500

    Fixed fns for Seger & Brockman wet/dry specialist comparison

--- src/clj/scratch.clj
+++ src/clj/scratch.clj
@@ -10,9 +10,8 @@
             ;[criterium.core :as crit]
             [utils.math :as um]
             [utils.misc :as msc]
-            [sauroprob.iterfreqs-fns :as fns]
             [sauroprob.plotly :as sp]
-            [sauroprob.iterfreqs-fns :as ifn]))
+            [sauroprob.iterfreqs-fns :as fns]))
 
 
 ;; Not really about chaos
@@ -22,38 +21,81 @@
 
 ;(clojure.repl/dir clojure.math)
 
-(defn mixed-expectation
-  [p]
-  (let [q (- 1 p)
-        dry-specialist-in-dry p
-        dry-specialist-in-wet (* q 0.6)
-        wet-specialist-in-wet q
-        wet-specialist-in-dry (* q 0.58)
-        wet-sum (+ dry-specialist-in-wet wet-specialist-in-wet)
-        dry-sum (+ wet-specialist-in-dry dry-specialist-in-dry)]
-    (* 1/2 (+ wet-sum dry-sum))))
+(comment
+  ;; wet specialist:
+  (/ (+ 1 0.58) 2) ; arithmetic 
+  (m/pow (* 1 0.58) 1/2) ; geometric
+  ;; dry specialist:
+  (/ (+ 1 0.6) 2) ; arithmetic
+  (m/pow (* 1 0.6) 1/2) ; geometric
+  )
 
-(defn mixed-variance
-  [p]
-  (let [q (- 1 p)
-        dry-specialist-in-dry p
-        dry-specialist-in-wet (* q 0.6)
-        wet-specialist-in-wet q
-        wet-specialist-in-dry (* q 0.58)
-        wet-sum (+ dry-specialist-in-wet wet-specialist-in-wet)
-        dry-sum (+ wet-specialist-in-dry dry-specialist-in-dry)
-        wet-sum-sq (* wet-sum wet-sum)
-        dry-sum-sq (* dry-sum dry-sum)]
-    (- (* 1/2 (+ wet-sum-sq dry-sum-sq))
-       (mixed-expectation p))))
+(def wet-in-wet-fit 1)
+(def wet-in-dry-fit 0.58)
+(def dry-in-wet-fit 0.6)
+(def dry-in-dry-fit 1)
 
-(defn mixed-geom
-  [p]
-  (let [q (- 1 p)
-        dry-specialist-in-dry p
-        dry-specialist-in-wet (* q 0.6)
-        wet-specialist-in-wet q
-        wet-specialist-in-dry (* q 0.58)
-        wet-sum (+ dry-specialist-in-wet wet-specialist-in-wet)
-        dry-sum (+ wet-specialist-in-dry dry-specialist-in-dry)]
-    (m/sqrt (* wet-sum dry-sum))))
+(def wet-env-relf 1/2)
+(def dry-env-relf (- 1 wet-env-relf))
+
+(defn wetdry-pop-summary
+  [summary-fn dry-specialist-relf]
+  (let [wet-specialist-relf (- 1 dry-specialist-relf)
+        ;; pop means:
+        wet-in-wet (* wet-specialist-relf wet-in-wet-fit)
+        wet-in-dry (* wet-specialist-relf wet-in-dry-fit)
+        dry-in-dry (* dry-specialist-relf dry-in-dry-fit)
+        dry-in-wet (* dry-specialist-relf dry-in-wet-fit)
+        wet-env-sum (+ dry-in-wet wet-in-wet)
+        dry-env-sum (+ wet-in-dry dry-in-dry)]
+    (summary-fn dry-env-relf dry-env-sum wet-env-sum)))
+
+(defn pop-arith-mean
+  [dry-env-relf dry-env-sum wet-env-sum]
+  (let [wet-env-relf (- 1 dry-env-relf)]
+    (+ (* dry-env-sum dry-env-relf)
+       (* wet-env-sum wet-env-relf))))
+
+(defn wetdry-pop-arith-mean
+  [dry-specialist-relf]
+  (wetdry-pop-summary pop-arith-mean dry-specialist-relf))
+
+(defn pop-geom-mean
+  [dry-env-relf dry-env-sum wet-env-sum]
+  (let [wet-env-relf (- 1 dry-env-relf)]
+    (* (m/pow dry-env-sum dry-env-relf)
+       (m/pow wet-env-sum wet-env-relf))))
+
+(defn wetdry-pop-geom-mean
+  [dry-specialist-relf]
+  (wetdry-pop-summary pop-geom-mean dry-specialist-relf))
+
+
+(comment
+  [(wetdry-pop-arith-mean 0.5) (wetdry-pop-arith-mean' 0.5)]
+)
+
+;; Wet specialist vs dry specialist--population arithmetic means rel to
+;; dry-specialist relf:
+(let [step 0.01
+      xs (range 0 (+ 1 step) step)
+      ys (map wetdry-pop-arith-mean xs)]
+  (-> (tc/dataset {:x xs :y ys})
+      (plotly/layer-line {:=x :x, :=y, :y})
+      plotly/plot
+      ;(assoc-in [:data 0 :line :width] 1)
+      ))
+
+;; Wet specialist vs dry specialist--population geometric means rel to
+;; dry-specialist relf:
+(let [step 0.01
+      xs (range 0 (+ 1 step) step)
+      ys (map wetdry-pop-geom-mean xs)]
+  (-> (tc/dataset {:x xs :y ys :x-vert [0.56 0.56] :y-vert [0.76 0.796]})
+      (plotly/layer-line {:=x :x, :=y, :y})
+      (plotly/layer-line {:=x :x-vert, :=y, :y-vert})
+      plotly/plot
+      ;(assoc-in [:data 0 :line :width] 1)
+      ))
+
+
